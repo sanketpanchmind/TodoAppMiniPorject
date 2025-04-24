4153a66fc4194c0a8a0924d49e998eb875954b48
Sanket <panchmind1@gmail.com>
2025-04-24 16:07:47 +0530

    feat: update Angular dependencies and add Material Design components
    - Added @angular/cdk and @angular/material dependencies to package.json.
    - Imported BrowserAnimationsModule in app.module.ts for Angular Material animations.
    - Increased z-index for login button in login.component.scss to ensure visibility.
    - Included MatSnackBarModule in login.module.ts for snackbar notifications.
    - Updated index.html to include Roboto font and Material Icons for better styling.
    - Modified styles.scss to set default font family to Roboto and ensure full height for html and body.

--- src/app/modules/beforeLogin/login/login.module.ts
+++ src/app/modules/beforeLogin/login/login.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { LoginComponent } from './login.component';
   imports: [
     CommonModule,
     LoginRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatSnackBarModule
   ]
 })
 export class LoginModule { }
